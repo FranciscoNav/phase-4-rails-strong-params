036453036dc81953965aa3302549f11b211cd066
Francisco <fnaveira11@gmail.com>
2021-06-15 22:44:27 -0400

    commit tree 8bacde090ae23c91326ecd70e143355bb4db941c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,8 +8,17 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
   # GET /birds/:id
